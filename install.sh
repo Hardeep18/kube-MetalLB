3e49362d372fa002c969b558acaf18aaf19777d3
Hardeep Singh <hardeep.singh18@outlook.com>
2020-11-04 00:29:36 +0530

    initial update

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
-# kube-MetalLB
-Set up MetalLB Load Balancing for Bare Metal Kubernetes
 
+#!/bin/bash
 
-```
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/namespace.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/metallb.yaml
-```
 
-```
 kubectl apply -f configmap.yml
-```
